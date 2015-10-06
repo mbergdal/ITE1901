74f1b22b46922caca7d4d058546fd9c7d8797b83
mbergdal@gmail.com <Vinter12>
2015-10-06 21:34:25 +0200

    Moved some things around

---
+++ src/src/com/headcrest/bstEx/AbstractTree.java
@@ -0,0 +1,9 @@
+package com.headcrest.bstEx;
+
+public abstract class AbstractTree<E extends Comparable<E>> implements Tree<E> {
+
+  @Override /** Return true if the tree is empty */
+  public boolean isEmpty() {
+    return getSize() == 0;
+  }
+}
